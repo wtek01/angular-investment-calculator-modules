fac6e0d274e01ee17009276b56bd64c519e1d5e2
Moh Med <gmelk02@gmail.com>
2024-08-28 11:45:44 +0200

    UserInputModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { InvestmentResultsModule } from './investment-results/investment-results.module';
+import { UserInputModule } from './user-input/user-input.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserInputComponent],
+  declarations: [AppComponent, HeaderComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, InvestmentResultsModule],
+  imports: [BrowserModule, InvestmentResultsModule, UserInputModule],
 })
 export class AppModule {}
