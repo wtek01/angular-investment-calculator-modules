a4ba11637e5453b9e122e83db7ebfa80c3b4be5c
Moh Med <gmelk02@gmail.com>
2024-08-28 10:47:30 +0200

    Migrating to Angular Module

---
+++ src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
